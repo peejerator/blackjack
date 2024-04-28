e66f3420de639da88a95c980109379ae6c09cec5
VoicesofNull <156015516+VoicesofNull@users.noreply.github.com>
2024-04-28 18:51:16 -0400

    Basics added
    Added backdrop.

---
+++ app/src/main/java/cop4546/group14/blackjack/HelpActivity.java
@@ -0,0 +1,16 @@
+package cop4546.group14.blackjack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cop4546.group14.blackjack.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
